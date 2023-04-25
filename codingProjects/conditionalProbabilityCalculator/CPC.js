3a3b79c001927516e69557d572fd1adc3416749f
cameronsi1996 <56313065+cameronsi1996@users.noreply.github.com>
2023-04-25 23:26:48 +0100

    Update CPC.js

--- codingProjects/conditionalProbabilityCalculator/CPC.js
+++ codingProjects/conditionalProbabilityCalculator/CPC.js
@@ -152,38 +152,3 @@ function calculate() {
 }
 
 
-
-
-function alcoholPrice() {
-
-    function roundToDec(num, dec) {
-    return +(Math.round(num + "e" + dec)  + "e-" + dec);
-    }
-
-    let cost = Number(document.getElementById("cost").value);
-    let alcoholPercentage = Number(document.getElementById("alcoholPercentage").value);
-    let vol = Number(document.getElementById("vol").value);
-    
-    let volAlcohol = vol * (alcoholPercentage / 100);
-    let costOfAlcohol = cost / volAlcohol;
-    /*console.log(costOfAlcohol);*/
-    let totalcostOfOther = cost * ((100 - alcoholPercentage) / 100);
-    /*console.log(cost);
-    console.log(alcoholPercentage);
-    console.log(totalcostOfOther);*/
-    
-    if (alcoholPercentage > 100) {
-    return (
-        document.getElementById("costOfAlcohol").innerHTML = "<i>Alcohol percentage entered incorrectly, please re-enter correct value.</i>",
-        document.getElementById("totalcostOfOther").innerHTML = "<i>Alcohol percentage entered incorrectly, please re-enter correct value.</i>",
-        document.getElementById("enjoyBanner").innerHTML = 'Please Try Again!!! <i class="fa-solid fa-triangle-exclamation"></i>'
-    )
-    } else {
-    return (
-        document.getElementById("costOfAlcohol").innerHTML = "<b>$" + roundToDec(costOfAlcohol, 2) + "</b> per ml of alcohol.",
-        document.getElementById("totalcostOfOther").innerHTML = "<b>$" + totalcostOfOther + "</b> out of $" + cost + " paid towards non-alcoholic content.",
-        document.getElementById("enjoyBanner").innerHTML = 'ENJOY!!! <i class="fa-regular fa-face-laugh-wink"></i>'
-    )
-    }
-}  
-
